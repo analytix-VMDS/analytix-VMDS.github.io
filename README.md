f77034f81c9f34ab230395e1227ecac646cbf58e
Zachary Sailer <zachsailer@gmail.com>
2015-06-21 21:20:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boilerplate code for a D3 application
+
+Clone this repository to quickly begin building a web application with D3 visualizations. 
+
+It contains all the boilerplate code for an application that uses the Backbone framework, Require module loading, and JQuery widgets/glue code. 
---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* 
+/ Main script that acts as the entry point for the application
+*/
+
+requirejs.config({
+    baseUrl:'',
+    path: {
+        backbone: 'lib/backbone/backbone-min',
+        bootstrap: 'lib/bootstrap/js/bootstrap.min',
+        d3: 'lib/d3/d3.min',
+        jquery: 'lib/jquery/jquery.min',
+        jqueryui: 'lib/jquery-ui/jquery-ui.min',
+        underscore: 'lib/underscore/underscore-min'
+    }
+})
+
+// Main application single entry point
+requirejs([ 
+    'jquery', 
+    'd3',
+    'js/network',
+    'js/cluster',
+],function($, d3, Network, Cluster) {
+        
+    
+});
