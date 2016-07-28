0ccbc71ccb5837b4c7ccbc633828aa9726210f5d
SMCCoder <sofie.christie@gmail.com>
2016-07-28 12:56:18 -0700

    added interating urllist

--- data-grabber.py
+++ data-grabber.py
@@ -3,6 +3,20 @@ import urllib.request
 import requests
 import os
 import json
+import cgitb
+import cgi
+from flask import Flask
+import pico
+#app = Flask(__name__)
+
+#@app.route('/index.html')#//localhost.8888')
+#def hello_world():
+#    return 'Hello, World!'
+
+#cgitb.enable() #This will show any errors on your webpage
+
+#inputs = cgi.FieldStorage() #REMEMBER: We do not have inputs, simply a button to run the program. In order to get inputs, give each one a name and call it by inputs['insert_name']
+
 
 #url = 'http://tidesandcurrents.noaa.gov/sltrends/mslUSTrendsTable.htm'
 #response = urllib.request.urlopen(url)
@@ -10,15 +24,29 @@ import json
 #text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
 #print(text)
 
-resp = requests.get('http://services.cngnow.com/V1/Stations.svc/external/circlefilter?latitude=35.4675&longitude=-97.5161&range=15&status=active')
+#def pyth:
 
-if resp.status_code != 200:
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+urls=['http://services.cngnow.com/V1/Stations.svc/external/circlefilter?latitude=35.4675&longitude=-97.5161&range=15&status=active',
+      'http://apiv3.iucnredlist.org/api/v3/species/countries/name/Loxodonta%20africana?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee',
+      'http://elections.huffingtonpost.com/pollster/api/charts.json']
 
-r = resp.json()
-data = json.dumps(r)
-print(data)
+#print(urls)
 
-newfile = open(os.path.join("json","test.json"), 'w')
-newfile.write(str(data))
-newfile.close()
+for index, urllist in enumerate(urls):
+    resp = requests.get(urllist)
+
+    print(resp)
+
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+    r = resp.json()
+    data = json.dumps(r)
+    #print(data)
+    print(index)
+
+    newfile = open(os.path.join("json","file"+str(index)+".json"), 'w')
+    newfile.write(str(data))
+    newfile.close()
+
+#pyth()
--- js/views/graph.js
+++ js/views/graph.js
@@ -12,32 +12,66 @@ define([
 
         initialize: function(){
 
-          /*this.element = "#svg_location"
+          this.element = "#svg_location"
 
           this.width = this.model.get("width");
           this.height = this.model.get("height");
 
           this.svg = d3.select(this.element).append("svg")
             .attr("width", this.width)
-            .attr("height", this.height);*/
+            .attr("height", this.height);
 
-          this.draw_graph();
+          this.get_data();
           this.draw_google_maps();
+
+          //console.log(u);
 
         },
 
-        draw_graph: function() {
-          d3.json("json/test.json", function(data){
-            console.log(data);
-          })
+        get_data: function() {
 
-          //d3.select("svg").style("background", "blue");
+          /*var data;
+          var scope = this;
+          d3.json("json/test.json", function(error, json){
 
+            if (error) {  //If error is not null, something went wrong.
+              console.log(error);  //Log the error.
+            } else {      //If no error, the file loaded correctly.
+              //console.log(json);   //Log the data.
+
+              //Include other code to execute after successful file load here
+              data = json;
+
+              //scope.draw_google_maps(data);
+
+            }
+          })*/
+
+        },
+
+        draw_bar_graph: function(keyname) {
+
+          var data = this.model.get("data");
+
+          //d3.select()
+
+        },
+
+        draw_pie_graph: function() {
+
+        },
+
+        draw_tree_graph: function() {
 
         },
 
         draw_google_maps: function() {
-          function initMap() {
+
+          //var z = location.pathname.substring(location.pathname.lastIndexOf('/')+1);
+          //alert(z);
+
+          console.log(this.model.get("datasets"));
+          /*function initMap() {
               // Create a map object and specify the DOM element for display.
               var map = new google.maps.Map(document.getElementById('map'), {
                   center: {
@@ -47,7 +81,21 @@ define([
                   scrollwheel: false,
                   zoom: 8
               });
-          }
+          }*/
+
+          // Create the Google Map…
+          var map = new google.maps.Map(d3.select("#map").node(), {
+            zoom: 8,
+            center: new google.maps.LatLng(37.76487, -122.41948),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+          });
+
+          $( "#map" ).resizable({
+              handles: 's',
+              stop: function(event, ui) {
+                  $(this).css("width", '');
+             }
+          });
         }
 
       });
